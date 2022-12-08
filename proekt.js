6ff9a7f533037724332d62403bf5048bf50de4b7
MariaKochetkova96 <khomandyakm.576@gmail.com>
2022-12-08 14:53:20 +0100

    work with PDR function

--- proekt.js
+++ proekt.js
@@ -1,3 +1,37 @@
+const PDFStart = nameRoute => {
+}
+const startPdf = () => {
+PDFStart('../media/r.pdf')
+}
+window.addEventListener('load', startPdf);
+
+let loadingTask = pdfjsLib.getDocument(nameRoute),
+pdfDoc = null,
+canvas = document.querySelector('#cv'),
+ctx = canvas.getContext('2d'),
+scale = 1.5,
+numPage = 1;
+
+loadingTask.promise.then(pdfDoc_ => {
+  pdfDoc = pdfDoc_;
+  document.querySelector('#cv').innerHTML = pdfDoc.numPages;
+  GeneratePDF(numPage)
+  });
+
+  const GeneratePDF = numPage => {
+    pdfDoc.getPage(numPage).then(page => {
+    let viewport = page.getViewport({ scale: scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    let renderContext = {
+    canvasContext : ctx,
+    viewport:  viewport
+    }
+    page.render(renderContext);
+    })
+    document.querySelector('#cv').innerHTML = numPage;
+    }
+
 function updateCvDetails(email, telefon, address, name, profession) {
   var cvEmail = document.querySelector("#cv-email")
   var cvTelefon = document.querySelector("#cv-tel-number")
@@ -146,12 +180,3 @@ workForm.addEventListener("submit", e => handleWorkSubmit(e))
   });
   reader.readAsDataURL(this.files[0]);
 });
-
-
-//window.jsPDF = window.jsPDF.jsPDF;
-
-//functio
-//updateCvPortrait(portrn generatePDF() {
-  //var proekt = new jsPDF()
-  //proekt.save("proekt.pdf")
-//}
